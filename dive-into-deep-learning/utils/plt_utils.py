909cb230eeec3178a4cbd9dcb665134eaaa018bf
lijiahui <lijiahuius@gmail.com>
2025-07-24 18:02:53 +0800

    add softmax regression

--- dive-into-deep-learning/utils/plt_utils.py
+++ dive-into-deep-learning/utils/plt_utils.py
@@ -1,3 +1,4 @@
+import torch
 from matplotlib import pyplot as plt
 from matplotlib_inline import backend_inline
 
@@ -63,3 +64,27 @@ def plot(X, Y=None, xlabel=None, ylabel=None, legend=None, xlim=None, ylim=None,
             axes.plot(y, fmt)
     
     set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
+    """
+    绘制图像列表
+    """
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        if torch.is_tensor(img):
+            # 图片张量
+            ax.imshow(img.numpy())
+        else:
+            # PIL图片
+            ax.imshow(img)
+        
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        
+        if titles:
+            ax.set_title(titles[i])
+    
+    return axes
